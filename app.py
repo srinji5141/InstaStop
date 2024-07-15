0cdddfef56d7389f75f3547da217b8c4e2baf35f
Srinjana Sriram <srinjana.sriram@gmail.com>
2024-07-15 16:25:00 +0800

    Some dialogue and control works

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from twilio.rest import Client
 from dotenv import load_dotenv
 import os
+from twilio.twiml.voice_response import VoiceResponse, Say
 
 app = Flask(__name__)
 
@@ -20,17 +21,21 @@ client = Client(account_sid, auth_token)
 @app.route("/", methods=['GET'])
 def make_outbound_call():
     call = client.calls.create(
-        url="http://demo.twilio.com/docs/voice.xml",
+        url="https://hello-voice-5480-udkzsk.twil.io/sacrifice", #Check out Twilio studio
         to="+6597487943",
         from_="+12512921238"
     )
     print(call.sid)
+    response = VoiceResponse()
+    response.say('Hello Beach!')
+    print(response)
     return "Outbound call initiated"
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-#Currently hosted on a local network, must be hosted globally
-#Must be able to change phone number
-#Must have dialogue and a timeout
-#Make notes
+#Must host globally
+#Must call only while opening
+#Get the dialogue to work
+#https://console.twilio.com/us1/service/functions/ZSf4b7b4c367e67255adcf6c916b6cf8de/runtime-functions-editor?frameUrl=%2Fconsole%2Ffunctions%2Feditor%2FZSf4b7b4c367e67255adcf6c916b6cf8de%2Fenvironment%2FZE0460a2fe0be41b8fa13888b09d372c8a%2Fconfig%2Fvariables%3Fapp_title%3Dhello-voice%26x-target-region%3Dus1&currentFrameUrl=%2Fconsole%2Ffunctions%2Feditor%2FZSf4b7b4c367e67255adcf6c916b6cf8de%2Fenvironment%2FZE0460a2fe0be41b8fa13888b09d372c8a%2Ffunction%2FZHb9555efe3ebd432e74bad943d7b1b79f%3F__override_layout__%3Dembed%26bifrost%3Dtrue%26x-target-region%3Dus1
+#Must be able to download shortcut, change phone number
